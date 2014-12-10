9715d6fa77885419d0c0d41b6c984f362aa0bec2
Nora Neyland <noraleeneyland@gmail.com>
2014-12-10 10:30:49 -0600

    12-10
    Add better faster method

--- 12-10.js
+++ 12-10.js
@@ -29,7 +29,20 @@ function semSort (versionsString) {
       }
     }
   });
-  
   return sorted.join("\n");
-  
+}
+
+
+//better version
+function semSort (versionsString) {
+  return versionsString.split("\n")
+  .sort(function (a,b)) {
+    var as = a.split('.'),
+    bs = b.split('.')
+
+    return (bs[0] - as[0]) ||
+            (bs[1] - as[1]) ||
+            (bs[2] - as[2])
+          }).join("\n")
+  }
 }
