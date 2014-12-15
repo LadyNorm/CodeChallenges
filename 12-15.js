da1cd749876aa7c408e3e29d86ffe8b402e70c38
Nora Neyland <noraneyland@Noras-MacBook-Pro.local>
2014-12-15 10:24:25 -0600

    fix indents

--- 12-15.js
+++ 12-15.js
@@ -4,17 +4,15 @@ function lookAndSay(n){
       function say(match, position) {
           return match.length.toString() + match.substring(0, 1);
       }
-
-      return num.toString().replace(look, say);
+  return num.toString().replace(look, say);
   }
-
   storage = [];
   storage.push(0);
   storage.push(1);
   var num = 1;
-  for (var i = 0; i < 30; i++) {
-      num = looking(num);
-      storage.push(num);
-  }
+    for (var i = 0; i < 30; i++) {
+        num = looking(num);
+        storage.push(num);
+    }
   return storage[n];
 }
