aefc6b76e13d8ffd7fe9dbac15c56bbbd1812ea9
Nora Neyland <noraleeneyland@gmail.com>
2014-12-16 15:42:52 +0000

    Given a string representation of a 2d map, return the number of islands in the map. Land spaces are denoted by a zero, while water is denoted by a dot. Two land spaces are considered connected if they are adjacent (but not diagonal).

---
+++ 12-16.js
@@ -0,0 +1,30 @@
+function countIslands (mapStr) {
+  
+  var counter =0;
+  
+  var first = mapStr.split("\n")
+  var array = [];
+  first.forEach(function(item){
+    array.push(item.split(""));
+  })
+  
+  for(var i=0;i<array.length;i++){
+    for (var j=0; j <array[i].length; j++){
+      if (array[i][j] == "0"){
+        if (i > 0 && array[i-1][j] !== "0" && j > 0 && array[i][j-1] !=="0"){
+          counter++;
+        }
+        else if( i === 0 && j > 0 && array[i][j-1] !== "0"){
+          counter++;
+        }
+        else if( j === 0 && i > 0 && array[i-1][j] !== "0"){
+          counter++;
+        }
+        else if (i === 0 && j === 0){
+          counter++;
+        } 
+      } 
+    }
+  } 
+  return counter;  
+}
